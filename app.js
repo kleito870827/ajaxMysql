359a33db8d6d95af302f7f8378a9ebfe2aa0717d
Yaziel Sanchez-Caleo <ysanchez-caleo@fvi.edu>
2016-08-04 22:34:55 -0400

    mysqlend

--- app.js
+++ app.js
@@ -1,11 +1,100 @@
+
 $.ajax({
   url: 'http://localhost:9999/tasks',
   success: function(resp, txt, xhr){
     var obj = JSON.parse(resp)
     console.log(obj);
     for (var i = 0; i < obj.length ; i++){
-        $('#new').append('<h4>'+ obj[i].description +'</h4>');
-    }
+      if(obj[i].status == 'new'){
+         $('#new').append('<h4>'+ obj[i].description+ ' -id '+obj[i].id +'</h4>');
+         $('#new').append(` <div class="btn-group" id="id`+ obj[i].id +`"role="group">
+                              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                SEND
+                                <span class="caret"></span>
+                              </button>
+                              <ul class="dropdown-menu">
+                                <li><a class="click" href="`+obj[i].id+`">STARTED</a></li>
+                                <li><a class="click" href="`+obj[i].id+`">COMPLETED</a></li>
+                                <li><a class="click" href="`+obj[i].id+`">DELETE</a></li>
+                              </ul>
+                            </div>`);
+      }else if (obj[i].status == 'started'){
+         $('#started').append('<h4>'+ obj[i].description+' -id '+obj[i].id +'</h4>');
+         $('#started').append(` <div class="btn-group" id="id`+ obj[i].id +`"role="group">
+                              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                SEND
+                                <span class="caret"></span>
+                              </button>
+                              <ul class="dropdown-menu">
+                                <li><a class="click" href="`+obj[i].id+`">NEW</a></li>
+                                <li><a class="click" href="`+obj[i].id+`">COMPLETED</a></li>
+                                <li><a class="click" href="`+obj[i].id+`">DELETE</a></li>
+                              </ul>
+                            </div>`);
+      }else{
+         $('#completed').append('<h4>'+ obj[i].description+' -id '+obj[i].id +'</h4>');
+         $('#completed').append(` <div class="btn-group" id="id`+ obj[i].id +`"role="group">
+                              <button id='prueba' type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                SEND
+                                <span class="caret"></span>
+                              </button>
+                              <ul class="dropdown-menu">
+                                <li><a class="click" href="`+obj[i].id+`">NEW</a></li>
+                                <li><a class="click" href="`+obj[i].id+`">STARTED</a></li>
+                                <li><a class="click" href="`+obj[i].id+`">DELETE</a></li>
+                              </ul>
+                            </div>`);
+      }
+
+    };
+    $(".click").on("click", function (){
+      event.preventDefault();
+      var change = $(this).text().toLowerCase();
+      var getId = $(this).attr("href");
+      if(change == "delete"){
+        $.ajax({
+          url: `http://localhost:9999/tasks/`+getId,
+          success: function(resp,txt,xhr){
+          console.log(resp);
+          },
+          method: 'DELETE'
+        })
+      }else{
+        $.ajax({
+        url: `http://localhost:9999/tasks/`+getId+`/`+change,
+        success: function(resp,txt,xhr){
+        console.log(resp);
+        },
+        method: 'PUT'
+      })
+      }
+
+      location.reload();
+    });
+    $("#create").on("click", function (){
+      var description = $("#description").val().toLowerCase();
+      var uid = $("#uid").val();
+      var status = $("#status").val().toLowerCase();
+      console.log(description+uid+status);
+      if(description != "" && uid != "" && status != ""){
+        if(status == "new" || status == "started" || status == "completed"){
+          $.ajax({
+            url: `http://localhost:9999/tasks/`+description+`/`+uid+`/`+status,
+            success: function(resp,txt,xhr){
+            console.log(resp);
+            },
+            method: 'POST'
+          })
+        }else{
+          alert("status new completed started");
+        }
+
+      }else{
+        alert("you have the text box empty");
+      }
+      location.reload();
+    });
 
   }
+
 });
